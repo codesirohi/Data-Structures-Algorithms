0d83e420752ffc30929a6cf7de0b1cacdd7cb747
codesirohi <siddhantshubham23@gmail.com>
2021-01-13 04:26:02 +0530

    4:25

---
+++ K_largest_elements.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#include<cstdio>
+using namespace std;
+
+int main(){
+
+    //find k largest element in a running stream
+    //you can stroe all the numbers
+    //worst case NlogK
+
+    int num;
+    while(scanf("%d",&num) != EOF){
+
+        cout<<num<<endl;
+    }
+
+    priority_queue<int,vector<int>,greater<int>> pq;
+
+
+
+
+    return 0;   
+}
--- pattern.cpp
+++ pattern.cpp
@@ -1,12 +1,18 @@
-#include<stdio.h>
-using name space std;
+#include<bits/stdc++.h>
+using namespace std;
 
 int main(){
 
-    for(int i=1; i  < n; i++){
-        for(int j= 1; j<i; j++){
-            cout<<"* ";
-        }
+    for(int i=0; i  < 10; i++){
+       cout<<i<<" ";
     }
-    retrun 0;
+
+    cout<<endl;
+    
+    for(int i=0; i  < 10; ++i){
+       cout<<i<<" ";
+    }
+
+
+    return 0;
 }
