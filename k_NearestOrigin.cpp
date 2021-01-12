ffb3a031dfcf0ea8e32127d6f270c972c377098a
codesirohi <siddhantshubham23@gmail.com>
2021-01-13 04:25:27 +0530

    k nearest new method added

--- k_NearestOrigin.cpp
+++ k_NearestOrigin.cpp
@@ -1,3 +1,45 @@
+//C++ using vector of pairs in stl
+
+
+
+class Solution {
+public:
+
+    
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) 
+    {
+        //declare a new vector v of pairs
+        vector<pair<int,int>> v;
+        
+        //n is the size of the vector points that
+        //have vectors of size 2 in it having x and y Cordinates
+        int n=points.size();
+        
+
+        for(int i=0;i<n;i++)
+        {
+            //int dist is x^2 + y^2
+            int dist = pow(abs(points[i][0]),2)+pow(abs(points[i][1]),2)
+
+            //here i is stored to keep note of the index of the point in 
+            //points vector so we can print it later on
+            v.push_back({dist,i});
+        }
+        //sorting the pair vector 
+        sort(v.begin().first,v.end().first);
+        
+        //declaring a new vector of vectors v1         
+        vector<vector<int>> v1;
+
+        for(int i=0;i<K;i++)
+        {
+          v1.push_back(points[v[i].second]);
+        }
+        return v1;
+    }
+};
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -40,9 +82,11 @@ class CarCompare{
         //method to overload () operator
         bool operator()(Car a, Car b){
             //code here
-            return a.dist()<b.dist();
+            return a.dist()>b.dist();
             //if distance of car a is less than distance of car b
             //this will return true
+            //if you want to  make a min head just
+            //change a.dist()<b.dist() to a.dist()<b.dist()
         
 
         }
@@ -56,6 +100,10 @@ int main(){
     //queue must be of type car
 
     priority_queue<Car,vector<Car>,CarCompare> pq;
+    int s;
+    cout<<"Enter the value of k: ";
+    cin>>s;
+    cout<<endl;
     
     
     //If you select a block of code and use the 
@@ -96,7 +144,7 @@ int main(){
     
 
 
-    while(!pq.empty()){
+    while(!pq.empty() && s--){
 
         Car p = pq.top();
         p.print();
