a7c8dd10d7e24c26b4f624b59e4dfefad35aee63
codesirohi <siddhantshubham23@gmail.com>
2021-01-16 00:20:41 +0530

    graph

--- generic_graph.cpp
+++ generic_graph.cpp
@@ -5,13 +5,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-template<typename T>;
+template<typename T>
 class Graph{
-
+    //using map output will be sorted
     map<T,list<T>> adjList;
     
 
 public:
+
+    //Graph_constructor 
     Graph(){
         
 
@@ -30,13 +32,50 @@ public:
 
     }
 
+    void bfs(T src){
+        //a queue 
+        queue<T> q;
+        //a hash map to maintain which nodes have been visited
+        map<T,bool> visited;
+
+        //pushed the source node in the queue
+        q.push(src);
+        //marked the source node as visited in the map
+        visited[src] =  true;
+
+        while(!q.empty()){
+
+            T node = q.front();
+            cout<<node<<" ";
+            q.pop();
+
+            //for the neighbors of the node
+            //check for the nodes which are not visited
+
+            for(T neighbour : adjList[node]){
+                //if the neighbour is not in the map " visited"
+                //push it into the queue
+                if(!visited[neighbour])
+                {
+                    q.push(neighbour);
+                    visited[neighbour] = true;
+                }
+                
+
+            }
+        }
+
+
+
+    }
+
     void printAdjList(){
         //iterate over a map
         for(auto i : adjList){
             cout<<i.first<<"-> ";
             //i.first is the key
             //i.second have the LL stored
-            for(int j: i.second){
+            for(auto j: i.second){
                 
                 cout<<j<<", ";
             }
@@ -46,20 +85,27 @@ public:
     }
 };
 
+ 
+
 int main(){
 
     //we dont havr to specify how many vertices
     //this graph will have bacause hash maps are dynamic
     
-    Graph g;
+    Graph<int> g;
 
-    g.addEdge("Putin","Trump",false);
-    g.addEdge("Putin","Modi",false);
-    g.addEdge("Putin","Trump",false);
-    g.addEdge("","");
-    g.addEdge("","");
+
+    g.addEdge(0,1);
+    g.addEdge(1,2);
+    g.addEdge(0,4);
+    g.addEdge(2,4);
+    g.addEdge(2,3);
+    g.addEdge(3,5);
+    g.addEdge(3,4);
     
-    g.printAdjList();
+    
+    g.bfs(0);
+    cout<<endl;
 
 
     return 0;
