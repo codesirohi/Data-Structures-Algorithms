b84e64065a9becae0b0855776a94f716a7a830af
codesirohi <siddhantshubham23@gmail.com>
2021-02-01 02:02:23 +0530

    min_coin_Bottom_UP

--- min_coin_needed.cpp
+++ min_coin_needed.cpp
@@ -1,33 +1,68 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int minCoin(int n, vectors<int> coins, int amount){
+int minCoin(int n, vector<int> coins, int amount){
 
 
     if(amount == 0) return 0;
 
-    int coinsNeeded = INT_MAX;
+    int ans = INT_MAX;
 
     for(int i = 0; i<n ; i++){
 
-        if(amount-coins[i] >= 0 )
+        if(amount-coins[i] >= 0 )   
         {
 
             int smallerAns = minCoin(n,coins,amount-coins[i]);
 
-            if(smallerAns != INT_MAX) {
+            if(smallerAns != INT_MAX) 
+            {
                 ans = min(ans, smallerAns+1);
             }
 
         }
 
+        
+
+    }
+    return ans;
+}
+
+
+
+int minCoinDP(int n, vector<int> coins, int amount) //bottom up
+{
+    vector<int> dp(amount,INT_MAX);
+
+    dp[0] = 0;
+    for(int rupee = 1; rupee<=amount; rupee++){
+        
+
+        //iterator oevr coins
+        for(int i = 0; i <n; i++){
+            
+            
+
+            if(coins[i]<=rupee){
+
+                int smallerAns = dp[rupee - coins[i]];
+                if(smallerAns<= INT_MAX){
+
+                    dp[rupee] = min(dp[rupee], smallerAns+1);
+
+                }
+
+            }
+        }
     }
 
-    
 
-
+    return dp[amount];
 
 }
+
+
+
 
 int main(){
 
@@ -35,18 +70,24 @@ int main(){
     cout<<"Enter No. of coins: ";
     int n;
     cin>>n;
-    vector<int> coins(n);
+    vector<int> coins;
     cout<<"Enter the values of coins: ";
     
-    for(auto i : coins){
-        cin>>i;
+    for(int i = 0; i<n; i++){
+        
+        int temp;
+        cin>>temp;
+        coins.push_back(temp);
+
     }
     cout<<"Enter the amount: ";
     int amount;
     cin>>amount;
 
-
-    cout<<minCoin(int n, vector<int> coins, int amount)<<endl;
+    // for(auto i:coins){
+    //     cout<<i<<" ";
+    // }
+    cout<<minCoinDP(n,coins, amount)<<endl;
 
 
     return 0;
