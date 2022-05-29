092acd3c97911dc02b5497b0f20e5a93a6180099
codesirohi <siddhantshubham23@gmail.com>
2022-05-29 18:02:26 +0530

    length of linked list

---
+++ lengthOfLinkedList.cpp
@@ -0,0 +1,16 @@
+int length( ListNode* head ){
+        
+        //base case
+        if( head == NULL ){
+            return 0;
+        }
+        
+        int i = 1;
+        
+        while( head -> next != NULL  ){
+            
+            head = head -> next;
+            i++;
+        }  
+        return i;
+    }  
